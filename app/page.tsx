d7b2a8ac909a7cafad2db33ab2dabc13f958f296
Muhammad Rizki Darmawan <muhrizdar@gmail.com>
2024-10-27 17:33:15 +0700

    add eslint config and prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-violet-700 font-black">Welcome to the world of Next.js</h1>
+    <h1 className="text-3xl font-black text-violet-700">
+      Welcome to the world of Next.js
+    </h1>
   );
 }
