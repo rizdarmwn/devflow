13efa89f04cabd227bff7749aabe4aeb748ed053
Muhammad Rizki Darmawan <muhrizdar@gmail.com>
2024-10-29 17:27:15 +0700

    add forms, mobile nav, and navlinks

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,4 @@
-import { auth, signOut } from "@/auth";
-import { Button } from "@/components/ui/button";
-import ROUTES from "@/constants/routes";
+import { auth } from "@/auth";
 
 export default async function Home() {
   const session = await auth();
@@ -9,16 +7,6 @@ export default async function Home() {
       <h1 className="text-3xl font-black text-violet-700">
         Welcome to the world of Next.js
       </h1>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button type="submit">Log out</Button>
-      </form>
     </>
   );
 }
