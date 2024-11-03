b90e7b63602acc3e14673731a609e4b1bbdfaa87
Muhammad Rizki Darmawan <muhrizdar@gmail.com>
2024-11-03 22:04:40 +0700

    fix close icon on tags

--- components/cards/TagCard.tsx
+++ components/cards/TagCard.tsx
@@ -36,18 +36,17 @@ const TagCard = ({
           <i className={`${iconClass} text-sm`}></i>
           <span>{name}</span>
         </div>
+        {remove && (
+          <Image
+            src="/icons/close.svg"
+            width={12}
+            height={12}
+            alt="close icon"
+            className="cursor-pointer object-contain invert-0 dark:invert"
+            onClick={handleRemove}
+          />
+        )}
       </Badge>
-
-      {remove && (
-        <Image
-          src="/icons/close.svg"
-          width={12}
-          height={12}
-          alt="close icon"
-          className="cursor-pointer object-contain invert-0 dark:invert"
-          onClick={handleRemove}
-        />
-      )}
 
       {showCount && (
         <p className="small-medium text-dark500_light700">{questions}</p>
